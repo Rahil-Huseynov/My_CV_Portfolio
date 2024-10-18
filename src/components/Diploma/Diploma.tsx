d10f5e2e96b083e2c5e3ba999e4a5d567dc3afc9
Rahil-Huseynov <rahil.huseynov.05@gmail.com>
2024-10-19 01:16:49 +0400

    fix style bug and fix diploma modal screen

--- src/components/Diploma/Diploma.tsx
+++ src/components/Diploma/Diploma.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from 'react';
 import diploma from '../../assets/award.png';
 import my_diploma from '../../assets/Diploma IT.png';
-import './Diploma.css'
+import './Diploma.css';
+
 const Diploma = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const toggleModal = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+    useEffect(() => {
+        if (isModalOpen) {
+            document.body.classList.add('no-scroll');
+        } else {
+            document.body.classList.remove('no-scroll');
+        }
+        return () => document.body.classList.remove('no-scroll');
+    }, [isModalOpen]);
+
     return (
         <>
             <section id='DIPLOMA'>
@@ -12,17 +28,34 @@ const Diploma = () => {
                         </div>
                         <div>
                             <h2 className='skills'>Diploma<span className='point'>.</span></h2>
-                            <p className='my_skills_about'>For me, a diploma represents dedication and the knowledge I've gained, marking my commitment to continuous learning.</p>
+                            <p className='my_skills_about'>
+                                For me, a diploma represents dedication and the knowledge I've gained, marking my commitment to continuous learning.
+                            </p>
                         </div>
                     </div>
                     <div className='all_my_education_container'>
-                        <img className='my_diploma' src={my_diploma} alt="my_diploma" />
+                        <img 
+                            className='my_diploma' 
+                            src={my_diploma} 
+                            alt="my_diploma" 
+                            onClick={toggleModal} 
+                            style={{ cursor: 'pointer' }}
+                        />
                     </div>
                 </div>
             </section>
             <div className='divide'></div>
+
+            {isModalOpen && (
+                <div className='modal_diploma' onClick={toggleModal}>
+                    <div className='modal_content_diploma' onClick={(e) => e.stopPropagation()}>
+                        <span className='close_button_diploma' onClick={toggleModal}>&times;</span>
+                        <img src={my_diploma} alt="my_diploma_large" className='modal_image_diploma' />
+                    </div>
+                </div>
+            )}
         </>
-    )
+    );
 }
 
-export default Diploma
+export default Diploma;
