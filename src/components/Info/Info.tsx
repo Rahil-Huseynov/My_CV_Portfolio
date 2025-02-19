45079ad442bd0c9e48d52b71dcd729f334dff7f9
Rahil-Huseynov <huseynov.rahil.05@gmail.com>
2025-02-20 00:49:02 +0400

    improve download cv

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -15,6 +15,17 @@ const Info: React.FC = () => {
     const [isNavbarOpen, setIsNavbarOpen] = useState<boolean>(false);
     const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
 
+
+    const handleDownloadCV = () => {
+        const link = document.createElement('a');
+        link.href = my_cv;
+        link.setAttribute('download', 'Rahil_Huseynov_CV.pdf');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
+
     const handleOpenModal = (): void => {
         setShowModal(true);
     };
@@ -155,7 +166,47 @@ const Info: React.FC = () => {
                             </div>
                             <div className='download_container'>
                                 <button className='contact_me' onClick={handleOpenModal}>CONTACT ME</button>
-                                <a className='Download_CV' href={my_cv} download rel="nofollow">Download CV</a>
+                                <button className="download-button" onClick={handleDownloadCV}>
+                                    <div className="docs">
+                                        <svg
+                                            viewBox="0 0 24 24"
+                                            width="20"
+                                            height="20"
+                                            stroke="black"
+                                            stroke-width="2"
+                                            fill="none"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            className="css-i6dzq1"
+                                        >
+                                            <path
+                                                d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"
+                                            ></path>
+                                            <polyline points="14 2 14 8 20 8"></polyline>
+                                            <line x1="16" y1="13" x2="8" y2="13"></line>
+                                            <line x1="16" y1="17" x2="8" y2="17"></line>
+                                            <polyline points="10 9 9 9 8 9"></polyline>
+                                        </svg>
+                                        Download CV
+                                    </div>
+                                    <div className="download">
+                                        <svg
+                                            viewBox="0 0 24 24"
+                                            width="24"
+                                            height="24"
+                                            stroke="white"
+                                            stroke-width="2"
+                                            fill="none"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            className="css-i6dzq1"
+                                        >
+                                            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                            <polyline points="7 10 12 15 17 10"></polyline>
+                                            <line x1="12" y1="15" x2="12" y2="3"></line>
+                                        </svg>
+                                    </div>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -204,8 +255,50 @@ const Info: React.FC = () => {
                                 </div>
                             </div>
                             <div className='download_container'>
-                                <button className='contact_me' onClick={handleOpenModal}>CONTACT ME</button>
-                                <a className='Download_CV' href={my_cv} download rel="nofollow">Download CV</a>
+                                <div className='download_container'>
+                                    <button className='contact_me' onClick={handleOpenModal}>CONTACT ME</button>
+                                    <button className="download-button" onClick={handleDownloadCV}>
+                                        <div className="docs">
+                                            <svg
+                                                viewBox="0 0 24 24"
+                                                width="20"
+                                                height="20"
+                                                stroke="black"
+                                                stroke-width="2"
+                                                fill="none"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                className="css-i6dzq1"
+                                            >
+                                                <path
+                                                    d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"
+                                                ></path>
+                                                <polyline points="14 2 14 8 20 8"></polyline>
+                                                <line x1="16" y1="13" x2="8" y2="13"></line>
+                                                <line x1="16" y1="17" x2="8" y2="17"></line>
+                                                <polyline points="10 9 9 9 8 9"></polyline>
+                                            </svg>
+                                            Download CV
+                                        </div>
+                                        <div className="download">
+                                            <svg
+                                                viewBox="0 0 24 24"
+                                                width="24"
+                                                height="24"
+                                                stroke="white"
+                                                stroke-width="2"
+                                                fill="none"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                className="css-i6dzq1"
+                                            >
+                                                <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                                                <polyline points="7 10 12 15 17 10"></polyline>
+                                                <line x1="12" y1="15" x2="12" y2="3"></line>
+                                            </svg>
+                                        </div>
+                                    </button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -214,7 +307,7 @@ const Info: React.FC = () => {
             </section>
 
             {showModal && <Modal_Contact onClose={handleCloseModal} isActive={showModal} />}
-            <div className='divide'></div>
+            <div className='divide_info'></div>
         </>
     )
 }
