a19a8ff1eed375be10aea772f77698ab28a639e9
Rahil-Huseynov <156452922+Rahil-Huseynov@users.noreply.github.com>
2025-04-27 22:46:56 +0400

    fix date

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,10 +1,12 @@
 import './Footer.css'
 const Footer = () => {
+    const date  = new Date()
+    const year:number = date.getFullYear()
     return (
         <>
         <div className="divide_footer"></div>
             <div className="copyright_container">
-                <p className="copyright">© Copyright 2024 Rahil Huseynov.</p>
+                <p className="copyright">© Copyright 2024 - {year} Rahil Huseynov.</p>
             </div>
         </>
     )
